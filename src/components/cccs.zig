6b49e0616c96c7345bf1bf5b82c8de86880aaaea
leventev <levente.vamos@protonmail.com>
2025-12-09 20:17:57 +0100

    remove obsolete rendering functions

--- src/components/cccs.zig
+++ src/components/cccs.zig
@@ -44,217 +44,6 @@ pub fn centerForMouse(pos: GridPosition, rotation: Rotation) GridPosition {
     return common.twoTerminalCenterForMouse(pos, rotation);
 }
 
-const radius = 25;
-
-pub fn render(
-    circuit_rect: dvui.Rect.Physical,
-    grid_pos: GridPosition,
-    rot: Rotation,
-    name: ?[]const u8,
-    value: ?GraphicComponent.ValueBuffer,
-    render_type: renderer.ElementRenderType,
-) void {
-    const pos = grid_pos.toCircuitPosition(circuit_rect);
-
-    const total_len = 2 * global.grid_size;
-    const diameter = 2 * radius;
-    const wire_len = (total_len - diameter) / 2;
-
-    const diamond_off = diameter / 2;
-
-    const render_colors = render_type.colors();
-    const thickness = render_type.thickness();
-
-    var buff: [256]u8 = undefined;
-    const value_str = if (value) |val| std.fmt.bufPrint(&buff, "{s}*I({s})", .{
-        val.cccs.multiplier_actual,
-        val.cccs.controller_name_actual,
-    }) catch unreachable else null;
-
-    switch (rot) {
-        .left, .right => {
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = pos,
-                .direction = .horizontal,
-                .pixel_length = wire_len,
-            }, render_type);
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = dvui.Point{
-                    .x = pos.x + global.grid_size * 2,
-                    .y = pos.y,
-                },
-                .direction = .horizontal,
-                .pixel_length = -wire_len,
-            }, render_type);
-
-            var path = dvui.Path.Builder.init(dvui.currentWindow().lifo());
-            defer path.deinit();
-
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + wire_len, .y = pos.y });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + wire_len + diamond_off, .y = pos.y - diamond_off });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + wire_len + 2 * diamond_off, .y = pos.y });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + wire_len + diamond_off, .y = pos.y + diamond_off });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + wire_len, .y = pos.y });
-
-            path.build().stroke(.{
-                .color = render_colors.component_color,
-                .thickness = thickness,
-            });
-
-            if (name) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 3,
-                        .y = pos.y - global.grid_size / 4,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            if (value_str) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + 2 * global.grid_size - global.grid_size / 3,
-                        .y = pos.y - global.grid_size / 4,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size - arrow_len / 2,
-                    .y = pos.y,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size + arrow_len / 2,
-                    .y = pos.y,
-                },
-                render_colors.component_color,
-                thickness,
-            );
-
-            const sign: f32 = if (rot == .right) 1 else -1;
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size + sign * arrow_len / 2,
-                    .y = pos.y,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size + sign * (arrow_len / 2 - arrowhead_len),
-                    .y = pos.y + arrowhead_len,
-                },
-                render_colors.component_color,
-                thickness,
-            );
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size + sign * arrow_len / 2,
-                    .y = pos.y,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x + global.grid_size + sign * (arrow_len / 2 - arrowhead_len),
-                    .y = pos.y - arrowhead_len,
-                },
-                render_colors.component_color,
-                thickness,
-            );
-        },
-        .top, .bottom => {
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = pos,
-                .direction = .vertical,
-                .pixel_length = wire_len,
-            }, render_type);
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = dvui.Point{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size * 2,
-                },
-                .direction = .vertical,
-                .pixel_length = -wire_len,
-            }, render_type);
-
-            var path = dvui.Path.Builder.init(dvui.currentWindow().lifo());
-            defer path.deinit();
-
-            path.addPoint(dvui.Point.Physical{ .x = pos.x, .y = pos.y + wire_len });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x - diamond_off, .y = pos.y + wire_len + diamond_off });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x, .y = pos.y + wire_len + 2 * diamond_off });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x + diamond_off, .y = pos.y + wire_len + diamond_off });
-            path.addPoint(dvui.Point.Physical{ .x = pos.x, .y = pos.y + wire_len });
-
-            path.build().stroke(.{
-                .color = render_colors.component_color,
-                .thickness = thickness,
-            });
-
-            if (name) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 2,
-                        .y = pos.y + global.grid_size / 3,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            if (value_str) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 2,
-                        .y = pos.y + 2 * global.grid_size - global.grid_size / 3,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size - arrow_len / 2,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size + arrow_len / 2,
-                },
-                render_colors.component_color,
-                thickness,
-            );
-
-            const sign: f32 = if (rot == .bottom) 1 else -1;
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size + sign * arrow_len / 2,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x + arrowhead_len,
-                    .y = pos.y + global.grid_size + sign * (arrow_len / 2 - arrowhead_len),
-                },
-                render_colors.component_color,
-                thickness,
-            );
-            renderer.drawLine(
-                dvui.Point.Physical{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size + sign * arrow_len / 2,
-                },
-                dvui.Point.Physical{
-                    .x = pos.x - arrowhead_len,
-                    .y = pos.y + global.grid_size + sign * (arrow_len / 2 - arrowhead_len),
-                },
-                render_colors.component_color,
-                thickness,
-            );
-        },
-    }
-}
-
 pub fn renderPropertyBox(
     inner: *cccs_module.Inner,
     value_buffer: *GraphicComponent.ValueBuffer,
@@ -301,19 +90,24 @@ pub fn mouseInside(
     circuit_rect: dvui.Rect.Physical,
     mouse_pos: dvui.Point.Physical,
 ) bool {
-    const pos = grid_pos.toCircuitPosition(circuit_rect);
-
-    const center: dvui.Point.Physical = switch (rotation) {
-        .left, .right => .{ .x = pos.x + global.grid_size, .y = pos.y },
-        .top, .bottom => .{ .x = pos.x, .y = pos.y + global.grid_size },
-    };
-
-    const xd = mouse_pos.x - center.x;
-    const yd = mouse_pos.y - center.y;
-
-    const check_radius = radius + 3;
-
-    return xd * xd + yd * yd <= check_radius * check_radius;
+    _ = grid_pos;
+    _ = rotation;
+    _ = circuit_rect;
+    _ = mouse_pos;
+    return false;
+    // const pos = grid_pos.toCircuitPosition(circuit_rect);
+    //
+    // const center: dvui.Point.Physical = switch (rotation) {
+    //     .left, .right => .{ .x = pos.x + global.grid_size, .y = pos.y },
+    //     .top, .bottom => .{ .x = pos.x, .y = pos.y + global.grid_size },
+    // };
+    //
+    // const xd = mouse_pos.x - center.x;
+    // const yd = mouse_pos.y - center.y;
+    //
+    // const check_radius = radius + 3;
+    //
+    // return xd * xd + yd * yd <= check_radius * check_radius;
 }
 
 const total_width = 2.0;
--- src/components/inductor.zig
+++ src/components/inductor.zig
@@ -41,157 +41,6 @@ pub fn centerForMouse(pos: GridPosition, rotation: Rotation) GridPosition {
     return common.twoTerminalCenterForMouse(pos, rotation);
 }
 
-const wire_pixel_len = 22;
-const middle_len = 2 * global.grid_size - 2 * wire_pixel_len;
-const circles = 3;
-const circle_diameter = 2 * (global.grid_size - wire_pixel_len) / circles;
-
-pub fn render(
-    circuit_rect: dvui.Rect.Physical,
-    grid_pos: GridPosition,
-    rot: Rotation,
-    name: ?[]const u8,
-    value: ?GraphicComponent.ValueBuffer,
-    render_type: renderer.ElementRenderType,
-) void {
-    const pos = grid_pos.toCircuitPosition(circuit_rect);
-
-    const inductor_color = render_type.colors().component_color;
-    const thickness = render_type.thickness();
-
-    var buff: [256]u8 = undefined;
-    const value_str = if (value) |val|
-        std.fmt.bufPrint(&buff, "{s}{s}", .{
-            val.inductor.actual,
-            bland.units.Unit.inductance.symbol(),
-        }) catch unreachable
-    else
-        null;
-
-    switch (rot) {
-        .left, .right => {
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = pos,
-                .direction = .horizontal,
-                .pixel_length = wire_pixel_len,
-            }, render_type);
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = dvui.Point{
-                    .x = pos.x + global.grid_size * 2,
-                    .y = pos.y,
-                },
-                .direction = .horizontal,
-                .pixel_length = -wire_pixel_len,
-            }, render_type);
-
-            var path = dvui.Path.Builder.init(dvui.currentWindow().lifo());
-            defer path.deinit();
-
-            for (0..circles) |i| {
-                const j = circles - (i + 1);
-                const offset: f32 = @as(f32, @floatFromInt(j * circle_diameter + circle_diameter / 2));
-                path.addArc(
-                    dvui.Point.Physical{
-                        .x = pos.x + wire_pixel_len + offset,
-                        .y = pos.y,
-                    },
-                    circle_diameter / 2,
-                    2 * dvui.math.pi,
-                    dvui.math.pi,
-                    false,
-                );
-            }
-
-            path.build().stroke(.{
-                .color = inductor_color,
-                .thickness = thickness,
-            });
-
-            if (name) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 3,
-                        .y = pos.y - global.grid_size / 4,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            if (value_str) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + 2 * global.grid_size - global.grid_size / 3,
-                        .y = pos.y - global.grid_size / 4,
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-        },
-        .bottom, .top => {
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = pos,
-                .direction = .vertical,
-                .pixel_length = wire_pixel_len,
-            }, render_type);
-            renderer.renderTerminalWire(renderer.TerminalWire{
-                .pos = dvui.Point{
-                    .x = pos.x,
-                    .y = pos.y + global.grid_size * 2,
-                },
-                .direction = .vertical,
-                .pixel_length = -wire_pixel_len,
-            }, render_type);
-
-            var path = dvui.Path.Builder.init(dvui.currentWindow().lifo());
-            defer path.deinit();
-
-            for (0..circles) |i| {
-                const j = circles - (i + 1);
-                const offset: f32 = @as(f32, @floatFromInt(j * circle_diameter + circle_diameter / 2));
-                path.addArc(
-                    dvui.Point.Physical{
-                        .x = pos.x,
-                        .y = pos.y + wire_pixel_len + offset,
-                    },
-                    circle_diameter / 2,
-                    dvui.math.pi / 2.0,
-                    -dvui.math.pi / 2.0,
-                    false,
-                );
-            }
-
-            path.build().stroke(.{
-                .color = inductor_color,
-                .thickness = thickness,
-            });
-
-            if (name) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 2,
-                        .y = pos.y + global.grid_size - (global.circuit_font_size / 2 + 8),
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-
-            if (value_str) |str| {
-                renderer.renderCenteredText(
-                    dvui.Point.Physical{
-                        .x = pos.x + global.grid_size / 2,
-                        .y = pos.y + global.grid_size + (global.circuit_font_size / 2 + 8),
-                    },
-                    dvui.themeGet().color(.content, .text),
-                    str,
-                );
-            }
-        },
-    }
-}
-
 pub fn renderPropertyBox(
     l: *Float,
     value_buffer: *GraphicComponent.ValueBuffer,
@@ -214,26 +63,31 @@ pub fn mouseInside(
     circuit_rect: dvui.Rect.Physical,
     mouse_pos: dvui.Point.Physical,
 ) bool {
-    const pos = grid_pos.toCircuitPosition(circuit_rect);
-
-    const tolerance = 3;
-
-    const rect: dvui.Rect.Physical = switch (rotation) {
-        .left, .right => dvui.Rect.Physical{
-            .x = pos.x + wire_pixel_len - tolerance,
-            .y = pos.y - circle_diameter / 2 - tolerance,
-            .w = middle_len + 2 * tolerance,
-            .h = circle_diameter / 2 + 2 * tolerance,
-        },
-        .bottom, .top => dvui.Rect.Physical{
-            .x = pos.x - tolerance,
-            .y = pos.y + wire_pixel_len - tolerance,
-            .w = circle_diameter / 2 + 2 * tolerance,
-            .h = middle_len + 2 * tolerance,
-        },
-    };
-
-    return rect.contains(mouse_pos);
+    _ = grid_pos;
+    _ = rotation;
+    _ = circuit_rect;
+    _ = mouse_pos;
+    return false;
+    // const pos = grid_pos.toCircuitPosition(circuit_rect);
+    //
+    // const tolerance = 3;
+    //
+    // const rect: dvui.Rect.Physical = switch (rotation) {
+    //     .left, .right => dvui.Rect.Physical{
+    //         .x = pos.x + wire_pixel_len - tolerance,
+    //         .y = pos.y - circle_diameter / 2 - tolerance,
+    //         .w = middle_len + 2 * tolerance,
+    //         .h = circle_diameter / 2 + 2 * tolerance,
+    //     },
+    //     .bottom, .top => dvui.Rect.Physical{
+    //         .x = pos.x - tolerance,
+    //         .y = pos.y + wire_pixel_len - tolerance,
+    //         .w = circle_diameter / 2 + 2 * tolerance,
+    //         .h = middle_len + 2 * tolerance,
+    //     },
+    // };
+    //
+    // return rect.contains(mouse_pos);
 }
 
 const total_width = 2.0;
