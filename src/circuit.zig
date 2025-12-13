91191461b4c6e996eb75d3875779c0af54a83792
leventev <levente.vamos@protonmail.com>
2025-12-13 15:08:40 +0100

    reduce radius of lumps

--- src/circuit.zig
+++ src/circuit.zig
@@ -397,7 +397,7 @@ pub const GraphicCircuit = struct {
         }
     }
 
-    pub const junction_radius = 0.11;
+    pub const junction_radius = 0.08;
 
     pub fn renderJunctions(
         self: *const GraphicCircuit,
